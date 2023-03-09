0612e20b0e3ac1b11246e14ecf4609f984cabad8
allxiao <arieshout@gmail.com>
2023-03-10 00:00:34 +0800

    add community coding style

---
+++ coding-practice-csharp/CommunityCodingStyle.cs
@@ -0,0 +1,23 @@
+﻿namespace coding_practice_csharp;
+
+public class CommunityCodingStyle
+{
+    public static int classStaticValue = 2;
+
+    private int fieldValue;
+
+    public CommunityCodingStyle(int val)
+    {
+        fieldValue = val;
+    }
+
+    public void javaStyleMethod() {
+    }
+
+    public void LocalVariableNaming()
+    {
+        int Variable1 = 3;
+        
+        Console.WriteLine("The value is " + Variable1);
+    }
+}
